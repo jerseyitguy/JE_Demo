d9e8920e0146c8d2144999480f4823bd3c98e368
JerseyITguy <72968468+jerseyitguy@users.noreply.github.com>
2026-09-07 19:04:27 +0100

    Refactor guessing loop to allow quitting

--- Guess.py
+++ Guess.py
@@ -1,14 +1,20 @@
 import random
 
 secret_number = random.randint(1, 100)
-guess = 0
 
-while guess != secret_number:
-    guess = int(input("Guess the number (1-100): "))
+while True:
+    user_input = input("Guess the number (1-100) or 'q' to quit: ")
+
+    if user_input.lower() == "q":
+        print(f"Game over! The number was {secret_number}.")
+        break
+
+    guess = int(user_input)
 
     if guess < secret_number:
         print("Too low!")
     elif guess > secret_number:
         print("Too high!")
-
-print("You win!")
+    else:
+        print("You win!")
+        break
