6986d1389b2202711eb2a89546983f88e6a31514
JerseyITguy <72968468+jerseyitguy@users.noreply.github.com>
2026-09-07 19:03:53 +0100

    Add feedback for low and high guesses
    Provide feedback on guesses before winning.

--- Guess.py
+++ Guess.py
@@ -6,4 +6,9 @@ guess = 0
 while guess != secret_number:
     guess = int(input("Guess the number (1-100): "))
 
+    if guess < secret_number:
+        print("Too low!")
+    elif guess > secret_number:
+        print("Too high!")
+
 print("You win!")
